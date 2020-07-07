984cb82087b1b47e8af1631bca05208c1dbcbf85
Harder-Ou <49508567+Harder-Ou@users.noreply.github.com>
2020-07-07 16:50:33 +0800

    Add files via upload

---
+++ scripts/basic.js
@@ -0,0 +1,35 @@
+/*
+let myHeading = document.querySelector('h1')
+myHeading.textContent = "Welcome to Ouyang's Page!"
+*/
+
+/*注释符号，成对使用，或者使用//
+let myVariable;
+myVariable = 'Ouyang';
+// let myVariable = 'Ouyang';
+myVariable = '欧阳';
+
+
+if...else条件语句
+let iceCream = 'chocolate';
+if (iceCream === 'chocolate') {
+    alert('我最喜欢巧克力冰激淋了。');
+} else {
+    alert('但是巧克力才是我的最爱呀……');
+}
+
+
+为两个参数进行乘法运算的函数
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+multiply(20,30)
+
+
+事件  点击页面任意位置，提示 别戳我 我怕疼
+document.querySelector('html').onclick = function () {
+    alert('别戳我，我怕疼。');
+}
+*/
